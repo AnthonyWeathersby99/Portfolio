1e19f45d21a1c52c248881cb2c1e7e5697f1e87e
root <root@Antdogs-PC.localdomain>
2024-12-16 01:39:11 -0800

    deploy test again

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  // Remove distDir as it might conflict with the output setting
 };
 
 module.exports = nextConfig;
